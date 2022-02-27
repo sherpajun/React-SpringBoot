bd7dffdeef502834a021cd64c08db6d7d849f995
Jun <87353476+sherpajun@users.noreply.github.com>
2022-02-27 19:23:47 +0900

    Add files via upload

---
+++ emax/src/main/java/com/Schurch/www/SmartChurchPjApplication.java
@@ -0,0 +1,13 @@
+package com.Schurch.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartChurchPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartChurchPjApplication.class, args);
+	}
+
+}
---
+++ emax/src/test/java/com/Schurch/www/SmartChurchPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Schurch.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartChurchPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
